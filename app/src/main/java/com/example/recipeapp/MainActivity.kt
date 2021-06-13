7c4a8f2a51937f09e0900d13ed344b15f871dde9
Matt Lenehan <mattlenehan@Matts-MBP.fios-router.home>
2021-06-13 16:14:49 -0400

    fix gradle versions

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey ya")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = "A Button")
+                }
+            }
+        }
     }
 }
