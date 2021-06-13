44d9d2bcc15a12b61ee43417ef547e618d00f0f0
Matt Lenehan <mattlenehan@Matts-MBP.fios-router.home>
2021-06-13 18:01:16 -0400

    clean up gradle settings again

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,30 +2,15 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text(text = "Hey ya")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(
-                    onClick = {}
-                ) {
-                    Text(text = "A Button")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
     }
+
+
 }
+
